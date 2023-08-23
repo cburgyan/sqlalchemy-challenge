84347936d2987156132ea671d3513061e00ef7a9
cburgyan <cburgyan@gmail.com>
2023-08-23 17:48:52 -0500

    change camel case to snake case

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -40,105 +40,105 @@ app = Flask(__name__)
 
 
 #################################################
-# Flask Routes
+# Flask _routes
 #################################################
-precipitationRoute = '/api/v1.0/precipitation'
-stationsRoute = '/api/v1.0/stations'
-tobsRoute = '/api/v1.0/tobs'
-tobsStartRoute = '/api/v1.0/<start>'
+precipitation_route = '/api/v1.0/precipitation'
+stations_route = '/api/v1.0/stations'
+tobs_route = '/api/v1.0/tobs'
+tobs_start_route = '/api/v1.0/<start>'
 
-tobsStartRouteStr = '/api/v1.0/&lt;start&gt;'
-tobsStartEndRoute = '/api/v1.0/<start>/<end>'
-tobsStartEndRouteStr = '/api/v1.0/&lt;start&gt;/&lt;end&gt;'
+tobs_start_route_str = '/api/v1.0/&lt;start&gt;'
+tobs_start_end_route = '/api/v1.0/<start>/<end>'
+tobs_start_end_route_str = '/api/v1.0/&lt;start&gt;/&lt;end&gt;'
 
 
 @app.route('/')
 def home():
-    routes = f"<br><a href='{precipitationRoute}'>{precipitationRoute}</a><br>\
-    <a href='{stationsRoute}'>{stationsRoute}</a><br>\
-    <a href='{tobsRoute}'>{tobsRoute}</a><br><br>\
+    routes = f"<br><a href='{precipitation_route}'>{precipitation_route}</a><br>\
+    <a href='{stations_route}'>{stations_route}</a><br>\
+    <a href='{tobs_route}'>{tobs_route}</a><br><br>\
     NOTE: Replace '&lt;start&gt;' in the URL with a date of the form YYYY-MM-DD<br>\
-    <a href='{tobsStartRoute}'>{tobsStartRouteStr}</a>  <br><br>\
+    <a href='{tobs_start_route}'>{tobs_start_route_str}</a>  <br><br>\
     NOTE: Replace '&lt;start&gt;' and '&lt;end&gt;' in the URL with a date of the form " \
              f"YYYY-MM-DD <br>\
-    <a href='{tobsStartEndRoute}'>{tobsStartEndRouteStr}</a>"
+    <a href='{tobs_start_end_route}'>{tobs_start_end_route_str}</a>"
 
     return "<h1>Home</h1>" + routes
 
 
-@app.route(precipitationRoute)
-def precipitationPage():
+@app.route(precipitation_route)
+def precipitation_page():
     # Perform a query to retrieve the data and precipitation scores
-    recent12Months = session.query(
+    recent12_months = session.query(
         measurement.date, measurement.prcp).\
         filter(measurement.date >= '2016-08-23').\
         order_by(measurement.date).\
         filter(measurement.prcp != None).\
         order_by(measurement.date).all()
 
-    precipDictionary = {}
-    for row in recent12Months:
-        if row[0] not in precipDictionary:
-            precipDictionary[row[0]] = []
-        precipDictionary[row[0]].append(row[1])
+    precip_dictionary = {}
+    for row in recent12_months:
+        if row[0] not in precip_dictionary:
+            precip_dictionary[row[0]] = []
+        precip_dictionary[row[0]].append(row[1])
 
-    return jsonify(precipDictionary)
-
-
-@app.route(stationsRoute)
-def stationPage():
-    stationsQuery = session.query(station.station, station.name).all()
-
-    stationsDictionary = {}
-    for row in stationsQuery:
-        if row[0] not in stationsDictionary:
-            stationsDictionary[row[0]] = row[1]
-
-    return jsonify(stationsDictionary)
+    return jsonify(precip_dictionary)
 
 
-@app.route(tobsRoute)
-def tobsPage():
-    mostActiveStations = session.query(
+@app.route(stations_route)
+def station_page():
+    stations_query = session.query(station.station, station.name).all()
+
+    stations_dictionary = {}
+    for row in stations_query:
+        if row[0] not in stations_dictionary:
+            stations_dictionary[row[0]] = row[1]
+
+    return jsonify(stations_dictionary)
+
+
+@app.route(tobs_route)
+def tobs_page():
+    most_active_stations = session.query(
         measurement.station,
         func.count(measurement.date)).\
         group_by(measurement.station).\
         order_by(func.count(measurement.date).desc()).all()
 
-    temperaturesMostActiveStation = session.query(
+    temperatures_most_active_station = session.query(
         measurement.date, measurement.tobs).\
-        filter(measurement.station == mostActiveStations[0][0]).\
+        filter(measurement.station == most_active_stations[0][0]).\
         filter(measurement.date > '2016-08-23').all()
 
 
-    tempDictionary = {}
-    for row in temperaturesMostActiveStation:
-        tempDictionary[row[0]] = row[1]
+    temp_dictionary = {}
+    for row in temperatures_most_active_station:
+        temp_dictionary[row[0]] = row[1]
 
-    return jsonify(tempDictionary)
+    return jsonify(temp_dictionary)
 
 
-@app.route(tobsStartRoute)
-def tobsStartPage(start, end=None):
-    tobsMinMaxAvg = session.query(
+@app.route(tobs_start_route)
+def tobs_start_page(start, end=None):
+    tobs_min_max_avg = session.query(
         func.min(measurement.tobs),
         func.max(measurement.tobs),
         func.avg(measurement.tobs)).\
         filter(measurement.date >= start).\
         filter(measurement.tobs != None).all()
 
-    print(tobsMinMaxAvg)
-    TMIN = tobsMinMaxAvg[0][0]
-    TMAX = tobsMinMaxAvg[0][1]
-    TAVG = tobsMinMaxAvg[0][2]
-    tempDict = {"TMIN": TMIN, "TMAX": TMAX, "TAVG": TAVG}
+    print(tobs_min_max_avg)
+    t_min = tobs_min_max_avg[0][0]
+    t_max = tobs_min_max_avg[0][1]
+    t_avg = tobs_min_max_avg[0][2]
+    temp_dict = {"TMIN": t_min, "TMAX": t_max, "TAVG": t_avg}
 
-    return jsonify(tempDict)
+    return jsonify(temp_dict)
 
 
-@app.route(tobsStartEndRoute)
-def tobsStartEndPage(start, end):
-    tobsMinMaxAvg = session.query(
+@app.route(tobs_start_end_route)
+def tobs_start_end_page(start, end):
+    tobs_min_max_avg = session.query(
         func.min(measurement.tobs),
         func.max(measurement.tobs),
         func.avg(measurement.tobs)).\
@@ -146,13 +146,13 @@ def tobsStartEndPage(start, end):
         filter(measurement.date <= end).\
         filter(measurement.tobs != None).all()
 
-    print(tobsMinMaxAvg)
-    TMIN = tobsMinMaxAvg[0][0]
-    TMAX = tobsMinMaxAvg[0][1]
-    TAVG = tobsMinMaxAvg[0][2]
-    tempDict = {"TMIN": TMIN, "TMAX": TMAX, "TAVG": TAVG}
+    print(tobs_min_max_avg)
+    t_min = tobs_min_max_avg[0][0]
+    t_max = tobs_min_max_avg[0][1]
+    t_avg = tobs_min_max_avg[0][2]
+    temp_dict = {"TMIN": t_min, "TMAX": t_max, "TAVG": t_avg}
 
-    return jsonify(tempDict)
+    return jsonify(temp_dict)
 
 
 if __name__ == "__main__":
